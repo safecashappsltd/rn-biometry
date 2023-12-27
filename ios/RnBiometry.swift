44a8ff0a326069811176df8e6b964cff9dcc6a75
Evgeny Rubin <evgeny.rubin@safecashapps.com>
2023-12-27 13:21:59 +0200

    chore: add encrypt decrypt ios

--- ios/RnBiometry.swift
+++ ios/RnBiometry.swift
@@ -1,5 +1,6 @@
 import LocalAuthentication
 import Security
+import CryptoKit
 
 @objc(RnBiometry)
 class RnBiometry: NSObject {
@@ -28,87 +29,131 @@ class RnBiometry: NSObject {
         }
     }
 
- @objc(showBiometricPromptForDecryption:resolver:rejecter:)
-    func showBiometricPromptForDecryption(params: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        let context = LAContext()
-        context.localizedCancelTitle = params["cancelButtonText"] as? String ?? "Cancel"
+@objc(showBiometricPromptForDecryption:resolver:rejecter:)
+func showBiometricPromptForDecryption(params: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+    let context = LAContext()
+    context.localizedCancelTitle = params["cancelButtonText"] as? String ?? "Cancel"
 
-        // Check if biometric authentication is available
-        var error: NSError?
-        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
-            reject("Biometric_Error", "Biometric authentication is not available", error)
-            return
-        }
+    // Check if biometric authentication is available
+    var error: NSError?
+    guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+        reject("Biometric_Error", "Biometric authentication is not available", error)
+        return
+    }
 
-        let promptMessage = params["promptMessage"] as? String ?? "Authenticate to decrypt the data"
-        let combinedString = params["encryptedToken"] as? String ?? ""
+    let promptMessage = params["promptMessage"] as? String ?? "Authenticate to decrypt and retrieve the data"
 
-        // Split the combined string into IV and encrypted data
-        let parts = combinedString.split(separator: ":")
-        guard parts.count == 2, let ivData = Data(base64Encoded: String(parts[0])), let encryptedData = Data(base64Encoded: String(parts[1])) else {
-            reject("Decryption_Error", "Invalid combined string format", nil)
-            return
-        }
+    context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: promptMessage) { success, evaluateError in
+        DispatchQueue.main.async {
+            if success {
+                // Retrieve the symmetric key from the keychain
+                let keyQuery: [String: Any] = [
+                    kSecClass as String: kSecClassGenericPassword,
+                    kSecAttrService as String: "YourService",
+                    kSecAttrAccount as String: "SymmetricKey",
+                    kSecReturnData as String: kCFBooleanTrue as Any,
+                    kSecMatchLimit as String: kSecMatchLimitOne
+                ]
 
-        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: promptMessage) { success, evaluateError in
-            DispatchQueue.main.async {
-                if success {
-                    // Implement the decryption logic here
-                    if let decryptedToken = self.decryptData(encryptedData: encryptedData, iv: ivData) {
-                        resolve(decryptedToken)
-                    } else {
-                        reject("Decryption_Error", "Decryption failed", nil)
-                    }
+                var item: CFTypeRef?
+                let status = SecItemCopyMatching(keyQuery as CFDictionary, &item)
+                if status == errSecSuccess, let keyData = item as? Data, let decryptedToken = decryptToken(encryptedToken, with: keyData) {
+                    resolve(decryptedToken)
                 } else {
-                    // Handle the error
-                    if let error = evaluateError as NSError? {
-                        reject("Authentication_Error", "Authentication failed", error)
-                    }
+                    reject("Keychain_Error", "Could not retrieve symmetric key", NSError(domain: NSOSStatusErrorDomain, code: Int(status)))
+                }
+            } else {
+                // Handle the error
+                if let error = evaluateError as NSError? {
+                    reject("Authentication_Error", "Authentication failed", error)
                 }
             }
         }
     }
+}
 
-   @objc(showBiometricPromptForEncryption:resolver:rejecter:)
-    func showBiometricPromptForEncryption(params: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        let context = LAContext()
-        context.localizedCancelTitle = params["cancelButtonText"] as? String ?? "Cancel"
 
-        // Check if biometric authentication is available
-        var error: NSError?
-        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
-            reject("Biometric_Error", "Biometric authentication is not available", error)
-            return
-        }
+  @objc(showBiometricPromptForEncryption:resolver:rejecter:)
+func showBiometricPromptForEncryption(params: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+    let context = LAContext()
+    context.localizedCancelTitle = params["cancelButtonText"] as? String ?? "Cancel"
 
-        let promptMessage = params["promptMessage"] as? String ?? "Authenticate to encrypt the data"
-        let payload = params["payload"] as? String ?? ""
+    // Check if biometric authentication is available
+    var error: NSError?
+    guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+        reject("Biometric_Error", "Biometric authentication is not available", error)
+        return
+    }
 
-        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: promptMessage) { success, evaluateError in
-            DispatchQueue.main.async {
-                if success {
-                    // Placeholder for encryption logic
-                    // Encrypt the payload here as needed
+    let promptMessage = params["promptMessage"] as? String ?? "Authenticate to encrypt and store the data"
+    let token = params["token"] as? String ?? ""
 
-                    // For now, just return true as you requested
-                    resolve(true)
-                } else {
-                    // Handle the error
-                    if let error = evaluateError as NSError? {
-                        reject("Authentication_Error", "Authentication failed", error)
-                    }
+    // Access Control for the keychain item
+    var accessControlError: Unmanaged<CFError>?
+    guard let accessControl = SecAccessControlCreateWithFlags(nil, kSecAttrAccessibleWhenUnlockedThisDeviceOnly, .userPresence, &accessControlError) else {
+        reject("AccessControl_Error", "Could not create access control", accessControlError?.takeRetainedValue())
+        return
+    }
+
+    context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: promptMessage) { success, evaluateError in
+        DispatchQueue.main.async {
+            if success {
+                let symmetricKey = generateSymmetricKey() // Implement this function
+                guard let encryptedToken = encryptToken(token, with: symmetricKey) else {
+                    reject("Encryption_Error", "Failed to encrypt the token", nil)
+                    return
+                }
+
+                // Store the symmetric key in the keychain
+                let keyQuery: [String: Any] = [
+                    kSecClass as String: kSecClassGenericPassword,
+                    kSecAttrService as String: "YourService",
+                    kSecAttrAccount as String: "SymmetricKey",
+                    kSecValueData as String: symmetricKey
+                ]
+                SecItemAdd(keyQuery as CFDictionary, nil)
+
+                // Return the encrypted token to JavaScript
+                resolve(encryptedToken)
+            } else {
+                // Handle the error
+                if let error = evaluateError as NSError? {
+                    reject("Authentication_Error", "Authentication failed", error)
                 }
             }
         }
     }
+}
 
-    private func decryptData(encryptedData: Data, iv: Data) -> String? {
-        // Implement decryption logic here
-        // Example: Decrypt using a symmetric key
+    private func generateSymmetricKey() -> Data {
+    let key = SymmetricKey(size: .bits256) // Generates a 256-bit key
+    return key.withUnsafeBytes { Data($0) }
+}
 
-        // For demonstration, return a placeholder string
-        return "Decrypted data"
+import CryptoKit
+
+private func encryptToken(_ token: String, with key: Data) -> Data? {
+    let key = SymmetricKey(data: key)
+    let data = Data(token.utf8)
+    do {
+        let sealedBox = try AES.GCM.seal(data, using: key)
+        return sealedBox.combined
+    } catch {
+        return nil
     }
+}
+
+
+private func decryptToken(_ encryptedToken: Data, with key: Data) -> String? {
+    let key = SymmetricKey(data: key)
+    do {
+        let sealedBox = try AES.GCM.SealedBox(combined: encryptedToken)
+        let decryptedData = try AES.GCM.open(sealedBox, using: key)
+        return String(data: decryptedData, encoding: .utf8)
+    } catch {
+        return nil
+    }
+}
 
     private func getBiometryType(context: LAContext) -> String {
         if #available(iOS 11.0, *) {
